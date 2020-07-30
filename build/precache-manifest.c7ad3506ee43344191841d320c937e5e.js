6d304da7bb4558e02cf8a58e72a5c1566fd798f8
chinnu143313 <69005258+chinnu143313@users.noreply.github.com>
2020-07-30 21:41:12 +0530

    Add files via upload

---
+++ build/precache-manifest.c7ad3506ee43344191841d320c937e5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ad4d7b23ea91552a35110fb85ded9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eeab1bb1df4ba84cc0c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d5d23440ac6a008b941b",
+    "url": "/static/js/2.dc702cc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dc702cc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eeab1bb1df4ba84cc0c",
+    "url": "/static/js/main.2d5f9934.chunk.js"
+  },
+  {
+    "revision": "0c88c9ae059e59d0ebbf",
+    "url": "/static/js/runtime-main.37a6d1fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
